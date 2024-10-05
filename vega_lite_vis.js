b459f411bd3227b974f1c92e7cffd4943ea4185b
KhanhShrimp <110376609+KhanhShrimp@users.noreply.github.com>
2024-10-06 04:03:31 +1100

    week 10 homework

---
+++ vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg_1 = "business_entry.vg.json";
+vegaEmbed("#business_entry_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "business_exit.vg.json";
+vegaEmbed("#business_exit_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
